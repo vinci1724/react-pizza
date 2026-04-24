dcacbabb44e803d8364a6175c38aac78aabb17af
vinci1724 <maksim.kondakov.0@gmail.com>
2026-04-24 16:52:12 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />}></Route>
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </div>
